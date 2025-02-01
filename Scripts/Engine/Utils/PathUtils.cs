960ee9bf598318ac23d825c7d649202fa1ddade1
Marik <136814357+VakulenkoMarik@users.noreply.github.com>
2025-02-01 22:28:53 +0200

    Fixing getting a file from a path

--- Scripts/Engine/Utils/PathUtils.cs
+++ Scripts/Engine/Utils/PathUtils.cs
@@ -1,21 +1,23 @@
+// ReSharper disable InconsistentNaming
 namespace Agario.Scripts.Engine.Utils;
 
 public static class PathUtils
 {
+    private const string programIniFilePath = "ProgramConf.ini";
+    private const string gameIniFilePath = "GameConf.ini";
+    
     public static string Get(string path)
     {
-        string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", ".."));
-                
-        return Path.Combine(projectRoot, path);
+        return Path.Combine(AppContext.BaseDirectory, path);
     }
-    
+
     public static string GetProgramIniFile()
     {
-        return Get("ProgramConf.ini");
+        return Get(programIniFilePath);
     }
-    
+
     public static string GetGameIniFile()
     {
-        return Get("GameConf.ini");
+        return Get(gameIniFilePath);
     }
 }
