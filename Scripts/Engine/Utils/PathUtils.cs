c10b2bfdfe9720285ca6e1249d55d8322eb60031
Marik <136814357+VakulenkoMarik@users.noreply.github.com>
2025-02-04 16:40:36 +0200

    Ini files to "Files" folder

--- Scripts/Engine/Utils/PathUtils.cs
+++ Scripts/Engine/Utils/PathUtils.cs
@@ -3,8 +3,8 @@ namespace Agario.Scripts.Engine.Utils;
 
 public static class PathUtils
 {
-    private const string programIniFilePath = "ProgramConf.ini";
-    private const string gameIniFilePath = "GameConf.ini";
+    private const string programIniFilePath = "Files\\ProgramConf.ini";
+    private const string gameIniFilePath = "Files\\GameConf.ini";
     
     public static string Get(string path)
     {
